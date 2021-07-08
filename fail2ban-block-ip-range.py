ef4095d22857eb39fe9ac835b255cbc537a631ad
Clemens Egger <clemens_e@gmx.net>
2021-07-08 23:51:13 +0200

    Change checked logfile to fail2ban.log
    By reading fail2ban.log all suspicious activities already detected by fail2ban
    are reconsidered to be blocked by an ip address range. This change requires
    also that different jails are taken into account.

--- fail2ban-block-ip-range.py
+++ fail2ban-block-ip-range.py
@@ -7,11 +7,11 @@ from collections import defaultdict
 
 # PART 1: system script call, filtering messages and IPs
 #
-# 1.1) this script searches for SASL LOGIN fails at last 10000 lines of mail.log
+# 1.1) this script searches for fail2ban.log for detections (1000 last lines)
 # 1.2) then it egreps the IPs, sort and uniq and count, and sort again.
 # 1.3) the IP result list is output to /tmp
 
-script = 'tail -n 10000 /var/log/mail.log | grep "SASL LOGIN authentication failed" | egrep "[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}" -o | sort | uniq -c | sort -nr > /tmp/top-ip-ranges'
+script = 'tail -n 1000 /var/log/fail2ban.log | grep -E "fail2ban.filter.*\[[0-9]+\]:.*\[[^]]+\] Found ([0-9]{1,3}\.){3}[0-9]{1,3}" -o | sed -re "s/fail2ban.filter\s+\[[0-9]+\]:\sINFO\s+\[//; s/\]//; s/Found //;" | sort | uniq -c > /tmp/top-ip-ranges'
 
 call(script, shell=True)
 
@@ -21,22 +21,23 @@ call(script, shell=True)
 file1 = open('/tmp/top-ip-ranges', 'r')
 Lines = file1.readlines()
 
-mylist = defaultdict(int)
-finalList = defaultdict(int)
+mylist = defaultdict(lambda: defaultdict(int))
+finalList = defaultdict(lambda: defaultdict(int))
 
 # 2.1) iterate ips and count
 for line in Lines:
     splitVect = line.strip().split(" ")
-    ip = splitVect[1].strip()
     count = splitVect[0].strip()
+    jail = splitVect[1].strip()
+    ip = splitVect[2].strip()
 
     # 2.2) iterate from cidr/32 down to 23 (descending)
     for cidr in range(32, 23, -1):
         ipnet  = IPv4Network(ip + "/" + str(cidr), False)
         index = str(ipnet.network_address) + "/" + str(cidr)
 
-        # 2.3) add the network and count of events into a dictionary
-        mylist[index] += int(count)
+        # 2.3) add the network, jail and count of events into a dictionary
+        mylist[jail][index] += int(count)
 
 # pprint(mylist)
 
@@ -45,8 +46,9 @@ for line in Lines:
 #
 for line in Lines:
     splitVect = line.strip().split(" ")
-    ip = splitVect[1].strip()
     count = splitVect[0].strip()
+    jail = splitVect[1].strip()
+    ip = splitVect[2].strip()
     maxCount = 0
     nextIndex = False
 
@@ -54,7 +56,7 @@ for line in Lines:
     for cidr in range(22, 33):
         ipnet  = IPv4Network(ip + "/" + str(cidr), False)
         index = str(ipnet.network_address) + "/" + str(cidr)
-        curCount = mylist[index]
+        curCount = mylist[jail][index]
         if(curCount >= maxCount):
             maxCount = curCount
             netIndex = index
@@ -66,15 +68,15 @@ for line in Lines:
 
     # 3.4 if netIndex is set and maxCount is above 10, add range to list
     if(netIndex and maxCount > 10):
-      finalList[netIndex] = maxCount
-
+      finalList[jail][netIndex] = maxCount
 
 #
 # PART 4: call fail2ban  (you can also call IPTABLES directly)
 #
-fail2ban_command = "fail2ban-client set postfix-sasl banip "
+fail2ban_command = "fail2ban-client set {} banip {}"
 
-for ip in finalList:
-    banIP_command = fail2ban_command + ip
-    # print(banIP_command)
-    call(banIP_command, shell=True)
+for jail in finalList:
+    for ip in finalList[jail]:
+        banIP_command = fail2ban_command.format(jail, ip)
+        #print(banIP_command)
+        call(banIP_command, shell=True)
