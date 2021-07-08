10fea30d4175fbdd4609d5946a8478631b6dd041
Clemens Egger <clemens_e@gmx.net>
2021-07-08 23:39:09 +0200

    Minor code clean up, fix indentation, add shebang

--- fail2ban-block-ip-range.py
+++ fail2ban-block-ip-range.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from pprint import pprint
 from subprocess import call
 from ipaddress import IPv4Network
@@ -14,7 +16,7 @@ script = 'tail -n 10000 /var/log/mail.log | grep "SASL LOGIN authentication fail
 call(script, shell=True)
 
 #
-# PART 2: reads the ip list detected and iteract:
+# PART 2: reads the ip list detected and iterate:
 #
 file1 = open('/tmp/top-ip-ranges', 'r')
 Lines = file1.readlines()
@@ -27,14 +29,14 @@ for line in Lines:
     splitVect = line.strip().split(" ")
     ip = splitVect[1].strip()
     count = splitVect[0].strip()
-    
-	# 2.2) iterate from cidr/32 down to 23 (descending)
-    for cidr in range(32, 23, -1):
-      ipnet  = IPv4Network(ip + "/" + str(cidr), False)
-      index = str(ipnet.network_address) + "/" + str(cidr)
 
-      # 2.3) add the network and count of events into a dictionary
-      mylist[index] += int(count)
+    # 2.2) iterate from cidr/32 down to 23 (descending)
+    for cidr in range(32, 23, -1):
+        ipnet  = IPv4Network(ip + "/" + str(cidr), False)
+        index = str(ipnet.network_address) + "/" + str(cidr)
+
+        # 2.3) add the network and count of events into a dictionary
+        mylist[index] += int(count)
 
 # pprint(mylist)
 
@@ -47,21 +49,21 @@ for line in Lines:
     count = splitVect[0].strip()
     maxCount = 0
     nextIndex = False
-    
+
 	# 3.2 iterate CIDR (now in ascending order)
     for cidr in range(22, 33):
-      ipnet  = IPv4Network(ip + "/" + str(cidr), False)
-      index = str(ipnet.network_address) + "/" + str(cidr)
-      curCount = mylist[index]
-      if(curCount >= maxCount):
-          maxCount = curCount
-          netIndex = index
+        ipnet  = IPv4Network(ip + "/" + str(cidr), False)
+        index = str(ipnet.network_address) + "/" + str(cidr)
+        curCount = mylist[index]
+        if(curCount >= maxCount):
+            maxCount = curCount
+            netIndex = index
 
-      # 3.3 if count decreases, than we've already got our best range
-      if(curCount < maxCount):
-          # found good network
-          continue
-		  
+        # 3.3 if count decreases, than we've already got our best range
+        if(curCount < maxCount):
+            # found good network
+            continue
+
     # 3.4 if netIndex is set and maxCount is above 10, add range to list
     if(netIndex and maxCount > 10):
       finalList[netIndex] = maxCount
@@ -73,6 +75,6 @@ for line in Lines:
 fail2ban_command = "fail2ban-client set postfix-sasl banip "
 
 for ip in finalList:
-  banIP_command = fail2ban_command + ip
-  # print(banIP_command)
-  call(banIP_command, shell=True)
+    banIP_command = fail2ban_command + ip
+    # print(banIP_command)
+    call(banIP_command, shell=True)
