12552ec3260de4f3bd13726727321dc49d78d4bb
Peter Bieringer <pb@bieringer.de>
2024-04-26 22:06:48 +0200

    major rework, discard call of external commands for retrieving log lines

--- fail2ban-block-ip-range.py
+++ fail2ban-block-ip-range.py
@@ -1,101 +1,156 @@
 #!/usr/bin/python3
+#
+# Scan fail2ban log and aggregate single banned IPv4 addresses into banned networks
+#
+# (P) & (C) 2021-2024 William Knak <williamknak@gmail.com>
+# (P) & (C) 2024-2024 Peter Bieringer <pb@bieringer.de>
 
 from pprint import pprint
 from subprocess import run, DEVNULL
 from ipaddress import IPv4Network
 from collections import defaultdict
-from tempfile import mkstemp
 import os
 import argparse
+import re
+from datetime import datetime
+
+file_default = '/var/log/fail2ban.log'
+maxage_default = '8h'
+countlimit_default = 7
 
 parser = argparse.ArgumentParser(
     prog='fail2ban-block-ip-range.py',
-    description='Scan /var/log/fail2ban.log and aggregate single banned IPs into banned networks'
+    description='Scan fail2ban log and aggregate single banned IPv4 addresses into banned networks',
+    epilog='Defaults: FILE=' + file_default + ' MAXAGE=' + maxage_default + ' COUNTLIMIT=' + str(countlimit_default)
 )
 
-parser.add_argument('-v', '--verbose', action='store_true')  # on/off flag
-parser.add_argument('-q', '--quiet', action='store_true')  # on/off flag
-parser.add_argument('--dry-run', action='store_true')  # on/off flag for dry-run mode
-parser.add_argument('--output-file', default='already_banned_ips.txt', help='File to store already banned IPs')
+parser.add_argument('-v', '--verbose'   , action='store_true')  # on/off flag
+parser.add_argument('-q', '--quiet'     , action='store_true')  # on/off flag
+parser.add_argument('-d', '--debug'     , action='store_true')  # on/off flag
+parser.add_argument('-D', '--dryrun'    , action='store_true')  # on/off flag
+parser.add_argument('-l', '--countlimit', action='store', type=int, default=countlimit_default)
+parser.add_argument('-f', '--file'      , action='store', type=str, default=file_default)
+parser.add_argument('-a', '--maxage'    , action='store', type=str, default=maxage_default)
 
 args = parser.parse_args()
 
-# PART 1: system script call, filtering messages and IPs
-#
-# 1.1) this script searches for fail2ban.log for detections (1000 last lines)
-# 1.2) then it egreps the IPs, sort and uniq and count, and sort again.
-# 1.3) the IP result list is output to /tmp
+fail2ban_log_file = args.file
+max_age = args.maxage
+countLimit = args.countlimit
 
-# Create temporary file, close it and let shell command write into it
-tmpf = mkstemp()
-os.close(tmpf[0])
+# convert max_age into seconds
+age_pattern = re.compile("^(\d+)([smhdw])$")
+m = age_pattern.match(max_age)
+seconds_per_unit = {
+    "s": 1,
+    "m": 60,
+    "h": 60 * 60,
+    "d": 60 * 60 * 24,
+    "w": 60 * 60 * 24 * 7
+}
 
-script = 'tail -n 10000 /var/log/fail2ban.log | grep -E "fail2ban.filter.*\[[0-9]+\]:.*\[[^]]+\] Found ([0-9]{1,3}\.){3}[0-9]{1,3}" -o | sed -re "s/fail2ban.filter\s+\[[0-9]+\]:\sINFO\s+\[//; s/\]//; s/Found //;" | sort | uniq -c>
-countLimit = 7
+if m:
+    max_age_seconds = int(m.group(1)) * seconds_per_unit[m.group(2)]
+    if args.debug:
+        print(f"Filter entries older {max_age} = {max_age_seconds}s")
+else:
+    print(f"MAXAGE not valid: {max_age}")
+    exit(1)
 
-run(script, shell=True)
-#
-# PART 2: reads the ip list detected and iterate:
-#
-file1 = open(tmpf[1], 'r')
-Lines = file1.readlines()
+dt_now = datetime.now()
 
+if not os.path.isfile(fail2ban_log_file):
+    print(f"File not found: {fail2ban_log_file}")
+    exit(1)        
+
+file = open(fail2ban_log_file, mode='r')
+
+fail2ban_log_pattern = re.compile("^([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}).* fail2ban.filter.*\[[0-9]+\]:.*\[([^]]+)\] Found ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})")
+
+myjailip = defaultdict(lambda: defaultdict(int))
 mylist = defaultdict(lambda: defaultdict(int))
 finalList = defaultdict(lambda: defaultdict(int))
 
-# 2.1) iterate ips and count
-for line in Lines:
-    splitVect = line.strip().split(" ")
-    count = splitVect[0].strip()
-    jail = splitVect[1].strip()
-    ip = splitVect[2].strip()
+# PART 1: filtering messages and IPs
+#
+# 1.1) this script searches for fail2ban.log for detections
+# 1.2) then it extracts the IPs after filter log line by age
+#
+# PART 2: reads the IP list detected and iterate
+#
+# 2.1) iterate IPs and count
 
-    # 2.2) iterate from cidr/32 down to 23 (descending)
-    for cidr in range(32, 23, -1):
-        ipnet = IPv4Network(ip + "/" + str(cidr), False)
-        index = str(ipnet.network_address) + "/" + str(cidr)
+while True:
+    line = file.readline()
+    m = fail2ban_log_pattern.search(line)
+    if m:
+        dt = datetime.fromisoformat(m.group(1))
+        timedate = m.group(1)
+        jail = m. group(2)
+        ip = m.group(3)
+        dt_delta = int((dt_now - dt).total_seconds())
+        if dt_delta > max_age_seconds:
+            if args.debug:
+                print(f"Found IPv4: {timedate} {dt_delta}s jail '{jail}' {ip} -> SKIP")
+            continue
 
-        # 2.3) add the network, jail and count of events into a dictionary
-        mylist[jail][index] += int(count)
+        if args.debug:
+            print(f"Found IPv4: {timedate} {dt_delta}s jail '{jail}' {ip} -> STORE")
+        myjailip[jail][ip] += 1
 
-# pprint(mylist)
+        # 2.2) iterate from cidr/32 down to 23 (descending)
+        for cidr in range(32, 23, -1):
+            ipnet = IPv4Network(ip + "/" + str(cidr), False)
+            index = str(ipnet.network_address) + "/" + str(cidr)
+
+            # 2.3) add the network, jail and count of events into a dictionary
+            mylist[jail][index] += 1
+
+    if not line:
+        break
+
+file.close()
+
+if args.debug:
+    print(f"List per jail/ip:")
+    pprint(myjailip)
+    print(f"List per jail/index:")
+    pprint(mylist)
+
 #
 # PART 3:  iterate IPs again, and get the best choice network range
 #
-for line in Lines:
-    splitVect = line.strip().split(" ")
-    count = splitVect[0].strip()
-    jail = splitVect[1].strip()
-    ip = splitVect[2].strip()
-    maxCount = 0
-    nextIndex = False
+for jail in myjailip:
+    for ip in myjailip[jail]:
+        maxCount = 0
+        nextIndex = False
 
-    # 3.2 iterate CIDR (now in ascending order)
-    for cidr in range(22, 33):
-        ipnet = IPv4Network(ip + "/" + str(cidr), False)
-        index = str(ipnet.network_address) + "/" + str(cidr)
-        curCount = mylist[jail][index]
-        if curCount >= maxCount:
-            maxCount = curCount
-            netIndex = index
+        # 3.2 iterate CIDR (now in ascending order)
+        for cidr in range(22, 33):
+            ipnet = IPv4Network(ip + "/" + str(cidr), False)
+            index = str(ipnet.network_address) + "/" + str(cidr)
+            curCount = mylist[jail][index]
+            if curCount >= maxCount:
+                maxCount = curCount
+                netIndex = index
 
-        # 3.3 if count decreases, than we've already got our best range
-        if curCount < maxCount:
-            # found good network
-            continue
+            # 3.3 if count decreases, than we've already got our best range
+            if curCount < maxCount:
+                # found good network
+                continue
 
-    # 3.4 if netIndex is set and maxCount is above the limit, add range to list
-    if netIndex and maxCount > countLimit:
-        if not netIndex.endswith("/32"):
-            finalList[jail][netIndex] = maxCount
+        # 3.4 if netIndex is set and maxCount is above the limit, add range to list
+        if netIndex and maxCount > countLimit:
+            if not netIndex.endswith("/32"):
+                finalList[jail][netIndex] = maxCount
 
-# delete temporary file
-os.remove(tmpf[1])
+if args.debug:
+    print(f"Final list of networks to block per jail:")
+    pprint(finalList)
 
 #
-# PART 4: call fail2ban  (you can also call IPTABLES directly)
+# PART 4: call fail2ban
 #
-
 fail2ban_command = "fail2ban-client set {} banip {}"
 fail2ban_get = "fail2ban-client get {} banned {}"
 
@@ -103,19 +158,25 @@ for jail in finalList:
     for ip in finalList[jail]:
         getban_command = fail2ban_get.format(jail, ip)
         banned = run(getban_command, capture_output=True, text=True, shell=True)
+        if banned.returncode != 0:
+            print(f"Unable to retrieve current status for jail '{jail}' {ip}: {banned.stderr}")
+            continue
+
         if banned.stdout.strip() == "0":
             banIP_command = fail2ban_command.format(jail, ip)
-            result = run(banIP_command, capture_output=True, text=True, shell=True)
-            if result.stdout.strip() == "1":
-                if not args.quiet:
-                    print(f"jail {jail} successful ban aggregated IP network: {ip}")
+            if not args.dryrun:
+                result = run(banIP_command, capture_output=True, text=True, shell=True)
+                if result.returncode != 0:
+                    print(f"Unable to ban for jail '{jail}' {ip}: {result.stderr}")
+                    continue
+
+                if result.stdout.strip() == "1":
+                    if not args.quiet:
+                        print(f"jail '{jail}' successful ban aggregated IPv4 network: {ip}")
+                else:
+                    print(f"jail '{jail}' unsuccessful try to ban aggregated IPv4 network: {ip} (result: {result.stdout.strip()})")
             else:
-                print(f"jail {jail} unsuccessful try to ban aggregated IP network: {ip} (result: {result.stdout.strip()})")
+                print(f"jail '{jail}' would ban aggregated IPv4 network: {ip} (dry-run)")
         else:
             if args.verbose:
-                print(f"jail {jail} aggregated IP network already banned: {ip}")
-
-# Print final aggregated list for reference
-if not args.quiet:
-    print("\nFinal Aggregated List:")
-    pprint(finalList)
+                print(f"jail '{jail}' aggregated IPv4 network already banned: {ip}")
